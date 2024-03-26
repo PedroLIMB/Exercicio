41dc4cd8b68108666a36647f934f23de52f72b94
Pedro Henrique <githublimb@gmail.com>
2024-03-26 13:17:37 -0300

    TestesFinais
    TestesFinais

--- BancoDosMano/banco_testes.py
+++ BancoDosMano/banco_testes.py
@@ -1,35 +1,36 @@
 import unittest
-from banco import Conta, SaldoInsuficiente
+from banco import Conta, Banco, SaldoInsuficiente
 
-#Testes Unitarios
 class TesteConta(unittest.TestCase):
     
-    
-    def teste_depositar(self):
-        conta = Conta("Cristiano Ronaldo", 100)
-        conta.depositar(50)
-        self.assertEqual(conta.consultar_saldo(), 150)
+    def setUp(self):
+        # Configuração inicial para cada teste na classe Conta
+        self.conta = Conta("Cristiano Ronaldo", 100)
 
-    def teste_Sacar(self):
-        conta = Conta("Cassio Ramos", 100)
-        conta.sacar(50)
-        self.assertEqual(conta.consultar_saldo(), 50)
+    def teste_depositar(self):
+        # Teste para verificar se o método depositar funciona corretamente
+        self.conta.depositar(50)
+        self.assertEqual(self.conta.consultar_saldo(), 150)
+
+    def teste_sacar(self):
+        # Teste para verificar se o método sacar funciona corretamente
+        self.conta.sacar(50)
+        self.assertEqual(self.conta.consultar_saldo(), 50)
 
     def teste_saldo_insuficiente(self):
-        conta = Conta("Bluzão", 100)
+        # Teste para verificar se uma exceção SaldoInsuficiente é levantada ao tentar sacar mais do que o saldo disponível
         with self.assertRaises(SaldoInsuficiente):
-            conta.sacar(150)
+            self.conta.sacar(150)
 
-#Teste de integração
 class TesteIntegracao(unittest.TestCase):
 
     def teste_criar_conta_depositar_sacar(self):
-        #Criar Conta
+        # Teste de integração para criar uma conta, depositar e sacar dinheiro
         conta = Conta("Pedro", 7)
-        #Depositar
+
         conta.depositar(50)
         self.assertEqual(conta.consultar_saldo(), 57)
-        #Sacar
+        
         conta.sacar(50)
         self.assertEqual(conta.consultar_saldo(), 7)
 
